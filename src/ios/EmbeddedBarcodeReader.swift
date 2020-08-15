bfe366da08c4b0a9a0e00ff0c13c81b7dda4bf8c
Fondue Port <reuben@codifi.us>
2020-08-14 20:35:11 -0400

    Updated to fix iOS build errors due to outdated Swift version

--- src/ios/EmbeddedBarcodeReader.swift
+++ src/ios/EmbeddedBarcodeReader.swift
@@ -2,11 +2,11 @@ import Foundation
 import AVFoundation
 
 @objc(EmbeddedBarcodeReader) class EmbeddedBarcodeReader: CDVPlugin, AVCaptureMetadataOutputObjectsDelegate {
-    
+
     class CameraPreview: UIView {
         var videoPreviewLayer:AVCaptureVideoPreviewLayer?
         var qrCodeFrameView:UIView?
-        
+
         func interfaceOrientationToVideoOrientation(_ orientation : UIInterfaceOrientation) -> AVCaptureVideoOrientation {
             switch (orientation) {
             case UIInterfaceOrientation.portrait:
@@ -21,7 +21,7 @@ import AVFoundation
                 return AVCaptureVideoOrientation.portraitUpsideDown;
             }
         }
-        
+
         override func layoutSubviews() {
             super.layoutSubviews();
             if let sublayers = self.layer.sublayers {
@@ -29,26 +29,26 @@ import AVFoundation
                     layer.frame = self.bounds;
                 }
             }
-            
-            self.videoPreviewLayer?.connection.videoOrientation = interfaceOrientationToVideoOrientation(UIApplication.shared.statusBarOrientation);
+
+            self.videoPreviewLayer?.connection?.videoOrientation = interfaceOrientationToVideoOrientation(UIApplication.shared.statusBarOrientation);
         }
-        
+
         func addPreviewLayer() {
             videoPreviewLayer?.frame = self.bounds
             self.backgroundColor = UIColor.clear
             self.layer.addSublayer(videoPreviewLayer!)
         }
-        
+
         func removePreviewLayer() {
             self.videoPreviewLayer?.removeFromSuperlayer()
             self.videoPreviewLayer = nil
         }
     }
-    
+
     var cameraPreview: CameraPreview!
     var captureSession:AVCaptureSession?
     var barcodeReadCallback:String?
-    
+
     var xPoint:CGFloat = 0.0
     var yPoint:CGFloat = 0.0
     var width:CGFloat = 0.0
@@ -56,22 +56,22 @@ import AVFoundation
     var lastRead:String = ""
     var lastTimeRead: Date = Date.init()
     var captureDevice: AVCaptureDevice?
-    
+
     override func pluginInitialize() {
         super.pluginInitialize()
         self.webView!.backgroundColor = UIColor.clear
         self.webView!.isOpaque = false
     }
-    
+
     func startReading(_ command: CDVInvokedUrlCommand) {
 //        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
         let cameraDefault = (command.arguments[4] as AnyObject!) as? String ?? "back"
-        
+
         if (cameraDefault == "front") {
             captureDevice = ((AVCaptureDevice.devices() as? [AVCaptureDevice])?
-                .filter({ $0.hasMediaType(AVMediaTypeVideo) && $0.position == .front}).first)!
+                .filter({ $0.hasMediaType(AVMediaType.video) && $0.position == .front}).first)!
         } else {
-            captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+            captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
         }
 
         do {
@@ -79,28 +79,28 @@ import AVFoundation
             yPoint = CGFloat((command.arguments[1] as AnyObject!).floatValue) + self.webView.frame.origin.y
             width = CGFloat((command.arguments[2] as AnyObject!).floatValue)
             height = CGFloat((command.arguments[3] as AnyObject!).floatValue)
-            
+
             // Get an instance of the AVCaptureDeviceInput class using the previous device object.
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+
             // Initialize the captureSession object.
             captureSession = AVCaptureSession()
-            
+
             // Set the input device on the capture session.
             captureSession?.addInput(input)
-            
+
             // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
             let captureMetadataOutput = AVCaptureMetadataOutput()
             captureSession?.addOutput(captureMetadataOutput)
-            
+
             // Set delegate and use the default dispatch queue to execute the call back
             captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
-            
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+
             // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
             cameraPreview = CameraPreview(frame: CGRect(x: xPoint, y: yPoint, width: width, height: height));
-            cameraPreview.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            cameraPreview.videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            cameraPreview.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            cameraPreview.videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
             //            videoPreviewLayer?.frame = view.layer.bounds
             self.webView!.backgroundColor = UIColor.clear
             self.webView!.isOpaque = false
@@ -108,50 +108,50 @@ import AVFoundation
             self.webView!.superview!.bringSubview(toFront: self.webView)
 //            self.webView!.superview!.sendSubview(toBack: cameraPreview!)
             cameraPreview.addPreviewLayer();
-            
+
             // Start video capture.
             captureSession?.startRunning()
-            
+
             // Move the message label and top bar to the front
             //            view.bringSubview(toFront: messageLabel)
             //            view.bringSubview(toFront: topbar)
-            
+
             // Initialize QR Code Frame to highlight the QR code
             cameraPreview.qrCodeFrameView = UIView()
-            
+
             cameraPreview.qrCodeFrameView?.layer.borderColor = UIColor.green.cgColor
             cameraPreview.qrCodeFrameView?.layer.borderWidth = 2
             self.webView?.addSubview(cameraPreview.qrCodeFrameView!)
             self.webView?.bringSubview(toFront: cameraPreview.qrCodeFrameView!)
-            
+
         } catch {
             // If any error occurs, simply print it out and don't continue any more.
             print(error)
         }
-        
+
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Camera Started")
         commandDelegate!.send(pluginResult, callbackId:command.callbackId)
     }
-    
+
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        
+
         var newReadTime: Date
         var milsSinceLastRead: Double
-        
+
         // Check if the metadataObjects array is not nil and it contains at least one object.
         if metadataObjects == nil || metadataObjects.count == 0 {
             cameraPreview.qrCodeFrameView?.frame = CGRect.zero
             return
         }
-        
+
         // Get the metadata object.
         let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        
-        if metadataObj.type == AVMetadataObjectTypeQRCode {
+
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
             // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
             let barCodeObject = cameraPreview.videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
             cameraPreview.qrCodeFrameView?.frame = CGRect.init(x: self.xPoint + barCodeObject!.bounds.minX, y: self.yPoint + barCodeObject!.bounds.minY, width: barCodeObject!.bounds.width, height: barCodeObject!.bounds.height)
-            
+
             if metadataObj.stringValue != nil {
                 if (self.barcodeReadCallback != nil) {
                     newReadTime = Date.init()
@@ -160,8 +160,8 @@ import AVFoundation
                         return
                     }
                     lastTimeRead = newReadTime
-                    lastRead = metadataObj.stringValue
-                    
+                    lastRead = metadataObj.stringValue!
+
                     let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: metadataObj.stringValue)
                     pluginResult!.setKeepCallbackAs(true)
                     commandDelegate!.send(pluginResult, callbackId:self.barcodeReadCallback)
@@ -169,28 +169,28 @@ import AVFoundation
             }
         }
     }
-    
-    func cameraWithPosition(position: AVCaptureDevicePosition) -> AVCaptureDevice? {
-        if let potentialDevices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) {
+
+    func cameraWithPosition(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        if let potentialDevices = AVCaptureDevice.devices(for: AVMediaType.video) as? [AVCaptureDevice] {
             for device in potentialDevices {
-                let device = device as! AVCaptureDevice
+                //let device = device as! AVCaptureDevice
                 if device.position == position {
                     return device
                 }
             }
         }
-        
+
         return nil
     }
-    
+
     //API methods for cordova
-    
+
     func startListening(_ command: CDVInvokedUrlCommand) {
         //add addListener
         print("listening")
         barcodeReadCallback = command.callbackId
     }
-    
+
     func stopReading(_ command: CDVInvokedUrlCommand) {
         //stop camera
         captureSession?.stopRunning()
@@ -200,26 +200,26 @@ import AVFoundation
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Camera Stopped")
         commandDelegate!.send(pluginResult, callbackId:command.callbackId)
     }
-    
+
     func hideCamera(_ command: CDVInvokedUrlCommand) {
         //hide camera
     }
-    
+
     func showCamera(_ command: CDVInvokedUrlCommand) {
         //show camera
     }
-    
+
     func switchCamera(_ command: CDVInvokedUrlCommand) {
         //switch camera
         if let session = captureSession {
             //Indicate that some changes will be made to the session
             session.beginConfiguration()
-            
+
             //Remove existing input
             let currentCameraInput: AVCaptureInput = (session.inputs.first as? AVCaptureInput)!
-            
+
             session.removeInput(currentCameraInput)
-            
+
             //Get new input
             var newCamera: AVCaptureDevice! = nil
             if let input = currentCameraInput as? AVCaptureDeviceInput {
@@ -229,7 +229,7 @@ import AVFoundation
                     newCamera = cameraWithPosition(position: .back)
                 }
             }
-            
+
             //Add input to session
             var err: NSError?
             var newVideoInput: AVCaptureDeviceInput!
@@ -239,33 +239,33 @@ import AVFoundation
                 err = err1
                 newVideoInput = nil
             }
-            
+
             if newVideoInput == nil || err != nil {
                 print("Error creating capture device input")
             } else {
                 session.addInput(newVideoInput)
             }
-            
+
             //Commit all the configuration changes at once
             session.commitConfiguration()
-            
+
             let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Camera Switched")
             commandDelegate!.send(pluginResult, callbackId:command.callbackId)
         } else {
             let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Couldn't switch")
             commandDelegate!.send(pluginResult, callbackId:command.callbackId)
         }
-        
+
     }
-    
+
     func getSupportedFlashModes(_ command: CDVInvokedUrlCommand) {
         //get supported flash modes
     }
-    
+
     func setFlashMode(_ command: CDVInvokedUrlCommand) {
         //set flash mode
     }
-    
+
     func setPreviewSize(_ command: CDVInvokedUrlCommand) {
         //set preview size
     }
